1501736f9c1ede3cdf2cd154fe6a609c2c989a9f
gabys barboza <gabrielly.barboza@outlook.com>
2020-09-11 20:00:12 -0300

    Updates

---
+++ precache-manifest.769baa5662526fbe372abad76e5df0c7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618a416049bba3fea720505fc2188174",
+    "url": "/dani-project/index.html"
+  },
+  {
+    "revision": "cf1eb7ba20b85a802ace",
+    "url": "/dani-project/static/css/main.1620efd0.chunk.css"
+  },
+  {
+    "revision": "86f189011995a47d7271",
+    "url": "/dani-project/static/js/2.78315757.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dani-project/static/js/2.78315757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1eb7ba20b85a802ace",
+    "url": "/dani-project/static/js/main.ead2a2f5.chunk.js"
+  },
+  {
+    "revision": "e7ec38c0cf9502aa1487",
+    "url": "/dani-project/static/js/runtime-main.76ca6ab2.js"
+  },
+  {
+    "revision": "d74ebbad7aa90d277b60cca006c8e8a8",
+    "url": "/dani-project/static/media/HRT.d74ebbad.png"
+  },
+  {
+    "revision": "4cf2b45e2e6379867a00ae41ec861fef",
+    "url": "/dani-project/static/media/banner-dani.4cf2b45e.jpg"
+  },
+  {
+    "revision": "52d1e0fd18178a30b734a677c2c24355",
+    "url": "/dani-project/static/media/carro-renault.52d1e0fd.png"
+  },
+  {
+    "revision": "6ce43a3a0ed0db303077d1516768626f",
+    "url": "/dani-project/static/media/foto1.6ce43a3a.png"
+  },
+  {
+    "revision": "a8df5641c6db482385f7ce2526a00f65",
+    "url": "/dani-project/static/media/foto2.a8df5641.png"
+  },
+  {
+    "revision": "72afb9bcc8f5dc57a9e4385cf90ebd2b",
+    "url": "/dani-project/static/media/foto3.72afb9bc.png"
+  },
+  {
+    "revision": "a2d749239f2e9374e9edb2a2ac3f654d",
+    "url": "/dani-project/static/media/logo.a2d74923.svg"
+  },
+  {
+    "revision": "81e813abee9399f246de95d6e20bf80b",
+    "url": "/dani-project/static/media/redbull.81e813ab.png"
+  },
+  {
+    "revision": "9838a37c7eccd45abeb35a62f40c4cef",
+    "url": "/dani-project/static/media/tororosso.9838a37c.png"
+  }
+]);
